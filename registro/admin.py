219bda22cf27613eccb88bd1182ab8b96f1f2c94
eticialima <leticalimacgi@gmail.com>
2024-11-17 16:40:39 -0300

    Aula 3 - Modelo

--- registro/admin.py
+++ registro/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from registro.models import (
+    Funcionario, ColetaFaces, Treinamento)
+ 
+class ColetaFacesInline(admin.StackedInline): 
+    model = ColetaFaces
+    extra = 0
 
-# Register your models here.
+class FuncionarioAdmin(admin.ModelAdmin):
+    readonly_fields = ['slug']
+    inlines = (ColetaFacesInline,) 
+    
+admin.site.register(Funcionario, FuncionarioAdmin)
+
+# admin.site.register(Funcionario)
+# admin.site.register(ColetaFaces) 
+
+admin.site.register(Treinamento)
